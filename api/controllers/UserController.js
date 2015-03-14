04ab8f52c793ca7be65ffef4490a23dad59537ae
Irene Chen <irene.chen@alumni.ubc.ca>
2015-03-14 10:27:04 -0700

    changes

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -16,8 +16,25 @@ module.exports = {
 	},
 
 	register: function(req, res) {
+		var email = req.param("email");
+		var password = req.param("password");
+		var confirm = req.param("confirm");
 
-	},
+		if (password != confirm) {
+			console.log("Passwords do not match");
+			return res.send(400, "Passwords do not match");
+		}
+
+		User.create({email: email, password: password}).exec(function(error, article) {
+			if(error){
+				console.log(error);
+				return res.send(400, error);
+			}
+			else {
+				console.log("Successfully created an account");
+			}
+		});
+	}
 
 	registerPost: function(req, res) {
 
