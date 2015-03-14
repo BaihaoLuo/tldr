5f15cc4efa2e1414a50514c4eb9f39c5e1bd523f
= <albert275@gmail.com>
2015-03-14 13:56:17 -0700

    basic blog model

---
+++ api/models/Blog.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  attributes: {
+
+    title: {
+      type: "string",
+      required: true
+    },
+
+    content: {
+      type: "string",
+      required: true
+    },
+    
+  }
+
+};
