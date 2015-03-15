6f02da3dd209ac3b1bc43834cc30ab2d140654bc
Irene Chen <irene.chen@alumni.ubc.ca>
2015-03-14 22:55:00 -0700

    c

---
+++ api/controllers/BookmarkController.js
@@ -0,0 +1,18 @@
+/**
+ * BookmarkController
+ *
+ * @description :: Server-side logic for managing the relationship between users and bookmarks
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+var bookmark = {}
+
+module.exports = {
+
+	bookmark:function(req, res) {
+		var user = User;
+    var article = Article;
+
+		bookmark[key(article)];
+	}
+};
